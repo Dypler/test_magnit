d3d383bfbed9de2610c8a278bf0e5d0a840bbc17
Dypler <sun.zakup@mail.ru>
2023-11-07 11:23:33 +0400

    final fix

--- src/js/main.js
+++ src/js/main.js
@@ -14,20 +14,35 @@ stickersWrapper.addEventListener('click', function () {
 const showAllStickersButton = document.getElementById('showAllStickers');
 const stickers = document.querySelectorAll('.stickers__item');
 let isShowing = false;
+let isWideScreen = window.innerWidth >= 1280;
 
-showAllStickersButton.addEventListener('click', function () {
-  isShowing = !isShowing;
-  const buttonText = isShowing ? 'Скрыть' : 'Посмотреть все Стикерсы';
-  const imgRotation = isShowing ? 'rotate180' : '';
-
-  stickers.forEach((sticker) => {
-    if (isShowing || !sticker.classList.contains('hidden')) {
+function updateStickerVisibility() {
+  stickers.forEach((sticker, index) => {
+    if (isShowing || (isWideScreen && index < 9) || (!isWideScreen && index < 6)) {
       sticker.style.display = 'inline-block';
     } else {
       sticker.style.display = 'none';
     }
   });
+}
+
+updateStickerVisibility();
+
+showAllStickersButton.addEventListener('click', function () {
+  isShowing = !isShowing;
+  const buttonText = isShowing ? 'Скрыть' : 'Посмотреть все Стикерсы';
+
+  updateStickerVisibility();
 
   showAllStickersButton.querySelector('img').classList.toggle('rotate180', isShowing);
   showAllStickersButton.querySelector('p').textContent = buttonText;
 });
+
+window.addEventListener('resize', function () {
+  isWideScreen = window.innerWidth >= 1280;
+  updateStickerVisibility();
+});
+
+
+
+
--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -2,59 +2,18 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   const swiper = new Swiper('.swiper', {
-//     direction: 'horizontal',
-//     loop: true,
-//     speed: 500,
-//     slidesPerView: 1,
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//   });
-
-//   const stickersItems = document.querySelectorAll(".stickers__item");
-
-//   stickersItems.forEach(function (item) {
-//     item.addEventListener("click", function (event) {
-//       event.preventDefault();
-//       const targetSlide = item.getAttribute("href");
-//       swiper.slideTo(targetSlide, 750, false);
-//     });
-//   });
-// });
-
-
 const swiper = new Swiper('.swiper', {
-
-  //! Основные настройки 
   direction: 'horizontal', // 'vertical', 'horizontal'
   loop: true, // true - круговой слайдер, false - слайдер с конечными положениями
   speed: 500, // скорость переключения слайдов
   effect: 'slider', // cards, coverflow, flip, fade, cube
-  // initialSlide: 2, // Начинаем со 2 слайдера
-  // freeMode: true, // можно перетаскивать как ленту
   slidesPerView: 1, // кол-во активных слайдов
-  // centeredSlides: true, // центрирование слайдов
   initialSlide: 0,
-  //! Пагинация (точки)
-  pagination: {
-      el: '.swiper-pagination',
-      clickable: true, // true - Пагинация становится кликабельной
-  },
-
-  //! Кнопки вперед и назад 
   navigation: {
       nextEl: '.s-button-next',
       prevEl: '.s-button-prev',
   },
 
-  //! Автоматическое перелистывание
-  // autoplay: {
-  //     delay: 1000, //Задержка перед перелистыванием 1с = 1000мл/с
-  // },
 });
 
 const stickersItems = document.querySelectorAll(".stickers__item");
@@ -66,14 +25,5 @@ stickersItems.forEach(function (item) {
     const targetSlide = document.getElementById(targetSlideId); // Найти соответствующий слайд
     const slideIndex = Array.from(targetSlide.parentElement.children).indexOf(targetSlide); // Найти индекс слайда
     swiper.slideTo(slideIndex, 1050); // Перейти к соответствующему слайду
-  });
-});
-const stickersWrapper = document.querySelector('.stickers__wrapper');
-const swiperContainer = document.getElementById('swiper-container');
-
-stickersWrapper.addEventListener('click', function () {
-  swiperContainer.scrollIntoView({
-    behavior: 'smooth', // Добавляет плавную прокрутку
-    block: 'start' // Прокручивает так, чтобы верхний край элемента был виден
   });
 });
